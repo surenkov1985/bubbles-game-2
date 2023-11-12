90e91116626265458128c80060f570bc332f2b48
Alexander <djz@yandex.ru>
2023-11-12 20:56:53 +0400

    add texture from images

--- src/assets.js
+++ src/assets.js
@@ -1,8 +1,10 @@
 
 module.exports = {
-    'bg-sheet0.png': {type: 'image', url: 'assets/images/bg-sheet0.png'},
-    'menu-bg.jpg': {type: 'image', url: 'assets/images/menu-bg.jpg'},
-    'menu-logo.png': {type: 'image', url: 'assets/images/menu-logo.png'},
-    'playbtn-sheet0.png': {type: 'image', url: 'assets/images/playbtn-sheet0.png'},
-    'aaa.png': {type: 'image', url: 'assets/images/aaa.png'},
+    // 'bg-sheet0.png': {type: 'image', url: 'assets/images/bg-sheet0.png'},
+    // 'menu-bg.jpg': {type: 'image', url: 'assets/images/menu-bg.jpg'},
+    // 'menu-logo.png': {type: 'image', url: 'assets/images/menu-logo.png'},
+    // 'playbtn-sheet0.png': {type: 'image', url: 'assets/images/playbtn-sheet0.png'},
+    // 'aaa.png': {type: 'image', url: 'assets/images/aaa.png'},
+
+    'bubble-game.json': {type: 'atlas', url: 'assets/textures/bubble-game.json'},
 };
